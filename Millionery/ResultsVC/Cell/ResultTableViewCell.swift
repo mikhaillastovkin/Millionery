0713446cc17560aac42aeb51e2e997a53b56dec4
Mikhail Lastovkin <m9503577335@gmail.com>
2022-02-01 20:59:27 +0300

    Добавил Делегат, Синглтон и Мементо

---
+++ Millionery/ResultsVC/Cell/ResultTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  ResultTableViewCell.swift
+//  Millionery
+//
+//  Created by Михаил Ластовкин on 01.02.2022.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var countCorrectAnswerLabel: UILabel!
+    @IBOutlet weak var summLabel: UILabel!
+    @IBOutlet weak var countClueLabel: UILabel!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+    func configureCell(result: GameResult) {
+        dateLabel.text = result.date
+        countCorrectAnswerLabel.text = result.countCorrectAnswer
+        summLabel.text = result.summ
+        countClueLabel.text = result.countClues
+    }
+    
+}
